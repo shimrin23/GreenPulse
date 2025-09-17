023e92074759896602555fb9f145f729948b0ec6
shimrin <139621909+shimrin23@users.noreply.github.com>
2025-09-18 02:24:58 +0530

    edited
    edited

--- frontend/src/services/treeService.js
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export const addTree = (formData) => api.post("/trees", formData);
-export const getTrees = () => api.get("/trees");
-export const getLeaderboard = () => api.get("/trees/leaderboard");
--- frontend/src/services/userService.js
+++
@@ -1,4 +0,0 @@
-import api from "./api";
-
-export const registerUser = (data) => api.post("/users/register", data);
-export const loginUser = (data) => api.post("/users/login", data);
